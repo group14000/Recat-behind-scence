cf6448a26252ffbd2555ca13bcaec842d1d8677b
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-10-10 19:00:53 +0530

    updated commit

--- project-file/src/App.js
+++ project-file/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
@@ -11,11 +12,11 @@ function App() {
   const toggleParagraphHandler = () => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
-
+ 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
